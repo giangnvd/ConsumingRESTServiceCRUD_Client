e7b2c64127efb9f48cd39f398693caffaf961202
Van Giang <giangnvd00570@fpt.edu.vn>
2018-12-18 10:10:00 +0700

    Success

---
+++ ConsumingRESTServiceCRUD_Client/Controllers/BookController.cs
@@ -0,0 +1,20 @@
+﻿using ConsumingRESTServiceCRUD_Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConsumingRESTServiceCRUD_Client.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            BookServiceClient bsc = new BookServiceClient();
+            ViewBag.listBooks = bsc.getAllBook();
+            return View();
+        }
+    }
+}
---
+++ ConsumingRESTServiceCRUD_Client/Models/BookServiceClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace ConsumingRESTServiceCRUD_Client.Models
+{
+    public class BookServiceClient
+    {
+        string BASE_URL = "http://localhost:53652/Service1.svc/rest/";
+        public List<Book> getAllBook()
+        {
+            var syncClient = new WebClient();
+            var content = syncClient.DownloadString(BASE_URL + "Books");
+            var json_serializer = new JavaScriptSerializer();
+            return json_serializer.Deserialize<List<Book>>(content);
+        }
+    }
+}
